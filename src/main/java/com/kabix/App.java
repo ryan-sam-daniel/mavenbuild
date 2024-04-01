7dd427392da757dc8212cf5868d3c6175d4dee27
Mr-A12k <kabil448446@gmail.com>
2024-04-01 11:33:24 -0700

    second commit

--- src/main/java/com/kabix/App.java
+++ src/main/java/com/kabix/App.java
@@ -4,37 +4,52 @@ package com.kabix;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       
-                int num1 = 10;
-                int num2 = 5;
-                
-                // Addition
-                int sum = num1 + num2;
-                System.out.println("Sum: " + sum);
-                
-                // Subtraction
-                int difference = num1 - num2;
-                System.out.println("Difference: " + difference);
-                
-                // Multiplication
-                int product = num1 * num2;
-                System.out.println("Product: " + product);
-                
-                // Division
-                if (num2 != 0) {
-                    double quotient = (double) num1 / num2;
-                    System.out.println("Quotient: " + quotient);
-                } else {
-                    System.out.println("Cannot divide by zero.");
-                }
-                
-                // Modulus
-                int remainder = num1 % num2;
-                System.out.println("Remainder: " + remainder);
-            }
-        }
+public class App {
+    int num1 = 5;
+    int num2 = 10;
 
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int division(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static int modulus(int num1, int num2) {
+        return num1 % num2;
+    }
+
+    public static void main(String[] args) {
+
+        // Addition
+
+        // // Subtraction
+        // int subtraction = num1 - num2;
+        // System.out.println("Difference: " + subtraction);
+
+        // // Multiplication
+        // int multiplication = num1 * num2;
+        // System.out.println("Product: " + multiplication);
+
+        // // Division
+        // if (num2 != 0) {
+        // double division = (double) num1 / num2;
+        // System.out.println("Quotient: " + division);
+        // } else {
+        // System.out.println("Cannot divide by zero.");
+        // }
+
+        // // Modulus
+        // int modulus = num1 % num2;
+        // System.out.println("Remainder: " + modulus);
+    }
+}
--- src/test/java/com/kabix/AppTest.java
+++ src/test/java/com/kabix/AppTest.java
@@ -1,20 +1,51 @@
 package com.kabix;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAddition() {
+
+        assertEquals(15, App.addition(10, 5));
+        assertEquals(0, App.addition(0, 0));
+        assertEquals(-5, App.addition(10, -15));
     }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(5, App.subtraction(10, 5));
+        assertEquals(0, App.subtraction(0, 0));
+        assertEquals(25, App.subtraction(10, -15));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(50, App.multiplication(10, 5));
+        assertEquals(0, App.multiplication(0, 0));
+        assertEquals(-150, App.multiplication(10, -15));
+    }
+
+    // @Test
+    // public void testDivision() {
+    // assertEquals(2.0, App.division(10, 5), 0.001);
+    // assertEquals(Double.POSITIVE_INFINITY, App.division(10, 0), 0.001);
+    // assertEquals(-0.666, App.division(10, -15), 0.001);
+    // }
+
+    // @Test
+    // public void testModulus() {
+    // assertEquals(0, App.modulus(10, 5));
+    // assertEquals(0, App.modulus(0, 1));
+    // assertEquals(10, App.modulus(10, 10));
+    // assertEquals(-5, App.modulus(10, -15));
+    // }
 }
